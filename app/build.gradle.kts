b334a22fae980222a4b1f44c3f59951ff05c02d2
Tupsi <tupsigh@tesira.org>
2025-05-25 19:59:00 +0200

    - improve layout for advance row - text size for familybonus is now dynamic - added new string value "Favorite" - increased version code to 16 and version name to 1.2.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.tesira.civic"
         minSdk = 28
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.2.11"
+        versionCode = 16
+        versionName = "1.2.12"
     }
 
     buildTypes {
