ed9013cb513bfffbbbf71fdd746f1c946494ac09
Tupsi <tupsigh@tesira.org>
2025-05-26 15:33:01 +0200

    - updated build version to 1.2.13 - enhanced toolbar and navigation drawer - added vector assets for menu icons - updated color resources - adjusted styles for buttons and dialogs - removed direct navigation from HomeFragment text views - refactored MainActivity for toolbar menu handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.tesira.civic"
         minSdk = 28
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.2.12"
+        versionCode = 17
+        versionName = "1.2.13"
     }
 
     buildTypes {
