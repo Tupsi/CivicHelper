09ecf8b9ac6982294e493f175e5177e70cdc9e09
Tupsi <tupsigh@tesira.org>
2025-05-27 22:28:05 +0200

    - Bumped version to 1.2.19 - Improved pinch-to-zoom and scrolling in TipsFragment:     - Added min and max text size for zooming     - Ensured scrolling works correctly during and after zoom gestures     - Made TextView focusable and clickable to better handle touch events - Updated layout for TipsFragment to include focusability and fadeScrollbars for the TextView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.tesira.civic"
         minSdk = 28
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.2.18"
+        versionCode = 21
+        versionName = "1.2.19"
     }
 
     buildTypes {
