57284d079d0581f8a7b5554bb1a111859f3badb2
Tupsi <tupsigh@tesira.org>
2025-06-03 16:12:54 +0200

    Converted `SettingsFragment.java` to Kotlin.

---
+++ app/src/main/kotlin/org/tesira/civic/SettingsFragment.kt
@@ -0,0 +1,18 @@
+package org.tesira.civic
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
