102626be469d6bff33e0e111b2b2ada7d530c453
Tupsi <tupsigh@tesira.org>
2025-06-03 21:42:04 +0200

    Convert `HomeSpecialsAdapter.java` to Kotlin.

---
+++ app/src/main/kotlin/org/tesira/civic/HomeSpecialsAdapter.kt
@@ -0,0 +1,56 @@
+package org.tesira.civic
+
+import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapter for showing a list of Special Abilities or Immunities against on the dashboard.
+ */
+class HomeSpecialsAdapter : RecyclerView.Adapter<HomeSpecialsAdapter.ViewHolder?>() {
+    private var localDataSet: Array<String>
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView? = view.findViewById<TextView?>(R.id.textView)
+    }
+
+    init {
+        this.localDataSet = arrayOf()
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_row_specials, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.textView!!.text = localDataSet[position]
+        viewHolder.textView.setBackgroundResource(R.drawable.specials_background)
+        if (viewHolder.textView.text.toString().startsWith("_")) {
+            viewHolder.textView.setTypeface(viewHolder.textView.typeface, Typeface.BOLD)
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount(): Int {
+        return localDataSet.size
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun submitSpecialsList(newSpecialsAndImmunities: MutableList<String>) {
+        this.localDataSet = newSpecialsAndImmunities.toTypedArray<String>()
+        notifyDataSetChanged()
+    }
+}
