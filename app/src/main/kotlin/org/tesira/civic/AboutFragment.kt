c8f8fbe83955a4dcfbe6a6a37a676530023230f1
Tupsi <tupsigh@tesira.org>
2025-06-04 08:35:05 +0200

    - Deleted `DialogCloseListener.kt`. - Updated `dialog_credits.xml` to use `tools:text` for previewing text values and `textIsSelectable` where appropriate. Increased text size for current and remaining credits. - Refactored `AboutFragment.kt` to simplify window insets handling and removed commented-out code.

--- app/src/main/kotlin/org/tesira/civic/AboutFragment.kt
+++ app/src/main/kotlin/org/tesira/civic/AboutFragment.kt
@@ -3,17 +3,14 @@ package org.tesira.civic
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
-import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
-
-import androidx.fragment.app.Fragment
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
-
+import androidx.fragment.app.Fragment
 import org.tesira.civic.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -49,20 +46,22 @@ class AboutFragment : Fragment() {
                 bottom = initialPaddingBottom + systemBarInsets.bottom
             )
 
-            WindowInsetsCompat.CONSUMED
+//            WindowInsetsCompat.CONSUMED
             windowInsets
         }
 
         val versionName = try {
-            requireActivity().packageManager.getPackageInfo(requireActivity().packageName, 0).versionName
-        } catch (e: PackageManager.NameNotFoundException) {
+            requireActivity().packageManager.getPackageInfo(
+                requireActivity().packageName,
+                0
+            ).versionName
+        } catch (_: PackageManager.NameNotFoundException) {
             "n/a"
         }
 
         binding.tvAboutVersion.text = getString(R.string.about_version, versionName)
-//        binding.tvAboutText.text = getString(R.string.about_text)
-        binding.tvAboutText.text = HtmlCompat.fromHtml(getString(R.string.about_text), HtmlCompat.FROM_HTML_MODE_LEGACY)
-//        binding.tvAboutText.movementMethod = ScrollingMovementMethod()
+        binding.tvAboutText.text =
+            HtmlCompat.fromHtml(getString(R.string.about_text), HtmlCompat.FROM_HTML_MODE_LEGACY)
         binding.tvAboutText.movementMethod = LinkMovementMethod.getInstance()
     }
 
