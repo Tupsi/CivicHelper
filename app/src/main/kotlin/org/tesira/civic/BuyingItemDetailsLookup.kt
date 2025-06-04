4043296373168b7c803dd08750e3ed2cddc1f71f
Tupsi <tupsigh@tesira.org>
2025-06-04 12:55:13 +0200

    - Converted `BuyingAdapter.java` to Kotlin. - Updated `BuyingItemDetails.kt` and `BuyingItemDetailsLookup.kt` to use non-nullable `String` for item details.

--- app/src/main/kotlin/org/tesira/civic/BuyingItemDetailsLookup.kt
+++ app/src/main/kotlin/org/tesira/civic/BuyingItemDetailsLookup.kt
@@ -5,8 +5,8 @@ import androidx.recyclerview.selection.ItemDetailsLookup
 import androidx.recyclerview.widget.RecyclerView
 
 class BuyingItemDetailsLookup(private val mRecyclerView: RecyclerView) :
-    ItemDetailsLookup<String?>() {
-    override fun getItemDetails(e: MotionEvent): ItemDetails<String?>? {
+    ItemDetailsLookup<String>() {
+    override fun getItemDetails(e: MotionEvent): ItemDetails<String>? {
         val view = mRecyclerView.findChildViewUnder(e.x, e.y)
         if (view != null) {
             val viewHolder = mRecyclerView.getChildViewHolder(view)
